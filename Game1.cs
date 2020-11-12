1e04d4c7363961b96110b6e4167cb18c3851cbec
Daniel-Marker <m012677j@student.staffs.ac.uk>
2020-11-12 20:28:58 +0000

    Basic sprite and model rendering
    Added goomba texture and fish model/texture.
    Updated Game1 to render the goomba and the two fish, with depth testing enabled

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,16 @@ namespace CNA_Graphics
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D goomba;
+        Vector2 position;
+
+        Texture2D fishTexture;
+        Model model;
+
+        float angle;
+
+        DepthStencilState depthStencilLessThan = new DepthStencilState() { DepthBufferEnable = true, DepthBufferFunction = CompareFunction.Less };
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,7 +28,9 @@ namespace CNA_Graphics
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            position = new Vector2(_graphics.PreferredBackBufferWidth/2, _graphics.PreferredBackBufferHeight/2);
+
+            angle = 0;
 
             base.Initialize();
         }
@@ -27,7 +39,10 @@ namespace CNA_Graphics
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            goomba = Content.Load<Texture2D>("goomba");
+
+            fishTexture = Content.Load<Texture2D>("fishTexture");
+            model = Content.Load<Model>("fish");
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,18 +50,51 @@ namespace CNA_Graphics
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(ClearOptions.DepthBuffer | ClearOptions.Target, Color.CornflowerBlue, 1.0f, 0);
 
-            // TODO: Add your drawing code here
+            GraphicsDevice.DepthStencilState = depthStencilLessThan;
+            GraphicsDevice.BlendState = BlendState.Opaque;
+            GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
+
+            angle += (float)gameTime.ElapsedGameTime.TotalSeconds * 30.0f;
+
+            Matrix world = Matrix.CreateRotationY(MathHelper.ToRadians(angle));
+
+            Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, (float)_graphics.PreferredBackBufferWidth / (float)_graphics.PreferredBackBufferHeight, 0.1f, 100.0f);
+
+            Matrix view = Matrix.CreateTranslation(2, 0, -3);
+
+            DrawModel(model, world, view, projection);
+            DrawModel(model, world * Matrix.CreateTranslation(2, 0, -2), view, projection);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(goomba, position, Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                    effect.Texture = fishTexture;
+                    effect.TextureEnabled = true;
+                }
+                mesh.Draw();
+            }
         }
     }
 }
